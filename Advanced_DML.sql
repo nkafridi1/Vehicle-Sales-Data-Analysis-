a83278b4834e1450d1cbaf1019344eeae201670a
Muhammad Danish Asim <masim@siammakro.co.th>
2024-12-13 22:48:33 -0500

    Advanced DML Added

--- Advanced_DML.sql
+++ Advanced_DML.sql
@@ -13,3 +13,33 @@ JOIN Sales s ON v.vin = s.vin
 JOIN MarketTrends mt ON v.vin = mt.vin
 WHERE (mt.mmr - s.sellingprice) > 5000
 ORDER BY price_difference DESC;
+
+-- Finding Vehicles with Above-Average Selling Price (First 10 records)
+SELECT v.make, v.model, s.sellingprice
+FROM Vehicles v
+JOIN Sales s ON v.vin = s.vin
+WHERE s.sellingprice > (
+    SELECT AVG(sellingprice)
+    FROM Sales
+) limit 10;
+
+-- Top 5 Most Sold Vehicle Models in Each State:
+SELECT state, make, model, total_sales
+FROM (
+    SELECT s.state, v.make, v.model, COUNT(*) AS total_sales,
+           RANK() OVER (PARTITION BY s.state ORDER BY COUNT(*) DESC) AS rank
+    FROM Sales s
+    JOIN Vehicles v ON s.vin = v.vin
+    GROUP BY s.state, v.make, v.model
+) ranked_sales
+WHERE rank <= 5;
+-- In Michigan:
+SELECT state, make, model, total_sales
+FROM (
+    SELECT s.state, v.make, v.model, COUNT(*) AS total_sales,
+           RANK() OVER (PARTITION BY s.state ORDER BY COUNT(*) DESC) AS rank
+    FROM Sales s
+    JOIN Vehicles v ON s.vin = v.vin
+    GROUP BY s.state, v.make, v.model
+) ranked_sales
+WHERE rank <= 5 and state = 'mi';
